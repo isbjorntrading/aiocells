cf57fac6d3d42093e78455cfd1b3fa1788bee060
Anders Lindstrom <anders@anderslindstrom.com>
2020-05-21 13:58:35 +1000

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (THIS_DIR / "README.md").read_text()
 
 setup(
     name="aiocells",
-    version="0.1.6",
+    version="0.1.7",
     python_requires=">=3.8",
     description="A package for synchronous and asynchronous"
                 " dependency graph computation",
