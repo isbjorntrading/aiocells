91a5deef41d2b75378334cf1facb2977818c82b1
Anders Lindstrom <anders@anderslindstrom.com>
2020-05-26 17:19:21 +1000

    Bumped patch version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (THIS_DIR / "README.md").read_text()
 
 setup(
     name="aiocells",
-    version="0.1.11",
+    version="0.1.12",
     python_requires=">=3.8",
     description="A package for synchronous and asynchronous"
                 " dependency graph computation",
