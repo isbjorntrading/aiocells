fda9a2222540e3c961669e9d70c900c80407ed95
Anders Lindstrom <anders@anderslindstrom.com>
2020-11-23 18:52:13 +1100

    * Bumped minor version
            modified:   setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (THIS_DIR / "README.md").read_text()
 
 setup(
     name="aiocells",
-    version="0.6.12",
+    version="0.7.0",
     python_requires=">=3.8",
     description="A package for synchronous and asynchronous"
                 " dependency graph computation",
